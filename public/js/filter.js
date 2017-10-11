c3b5162929f23b59c892ae942e0577445a01b5f6
nancyzj <ancizj@hotmail.com>
2017-10-10 22:04:52 -0500

    cree un filtro, para el nombre de las imagenes, porque causaba conflicto, y usamos una pequeña expresion regular

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,5 +1,17 @@
 (function () {
   angular.module('pokedex.filters', [])
+  	.filter('normalize', function(){
+  		return function(input) {
+  			input = input
+  					.replace('♀', 'f')
+  					.replace('♂','m')
+  					/*crear una expresion regular que a los caracteres
+  					especiales los remplace por una cadena de texto blanca*/
+  					.replace(/\W+/g,'');
+  			// esta funcion debe de retonrnar el resultado
+  			return input.toLowerCase();
+  		}
+  	})
     .filter('imageify', function () {
       return function (input) {
         var url = "img/pokemons/" + input.toLowerCase() + ".jpg";
