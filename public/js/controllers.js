9d9e0045ff32aa2ec8fb3129e2386c4aad341d34
nancyzj <ancizj@hotmail.com>
2017-10-15 00:37:06 -0500

    agregamos filtros, de js y realizamos otra promesa para filtrar la entrada (nombre), del pikachu

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -10,27 +10,14 @@
 		});		
 	}])
 	/*Crear nuestro controlador, nombre y funcion*/
-	.controller('PokemonController',['$scope', function($scope){
-		// crearemos nuestro objeto de pokemon
-		$scope.pokemon = {
-			id:001,
-			name: 'Bulbasaur',
-			species:'Seed Pokemon',
-			type: ['Grass', 'Poison'],
-			height: '2.4',
-			weight: '15.2 lib',
-			abilities: ['Overgrow','Chlorophyll','Water Stream'],
-			state:{
-				hp:45,
-				attack:49,
-				defense:49,
-				"sp.atk":65,
-				"sp.def":65,
-				speed:45,
-				total:318
-			},
-			evolution:["Bulbasaur", "Ivysaur","Venusaur"]
-		};
+	.controller('PokemonController',['$scope','pokemonServices', function($scope, pokemonServices){
+		$scope.pokemon = {};
+
+		// utilizar el servicio 
+		pokemonServices.byName('mew')
+		.then(function(data) {
+			$scope.pokemon = data;
+		});	
 	}])
 	// nuevo controlador para las Tabs
 	.controller('TabsController', function(){
--- public/js/services.js
+++ public/js/services.js
@@ -1,6 +1,9 @@
 (function(){
 	angular.module('pokedex.services',[])
-	.factory('pokemonServices', ['$http','$q', function($http, $q) {
+	.factory('pokemonServices', ['$http','$q','$filter', function($http, $q, $filter) {
+		// obtenemos el filtro que creamos
+		var normalize = $filter('normalize');
+
 		function all() {
 			var deferred = $q.defer();
 
@@ -10,9 +13,29 @@
 			});
 			return deferred.promise;
 		}
+		// funcion para filtrar por nombre, usando filter de js
+		function byName(name) {
+			name = normalize(name);
+			var deferred = $q.defer();
 
+			// invocamos a nuestra funcion que contiene la data
+			all().then(function (data) {
+				var results = data.filter(function(pokemon){
+					return normalize(pokemon.name) === name;
+				});
+				if(results.length > 0) {
+					deferred.resolve(results[0]);
+				} else {
+					// si falla, que rechace la promesa
+					deferred.reject();
+				}
+			});
+			return deferred.promise;
+		}
+		// retornamos en nuestro objetos las funciones a usar
 		return {
-			all:all
+			all:all,
+			byName: byName
 		};
 	}]);
 })();
